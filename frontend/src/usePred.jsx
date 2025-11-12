1fa6ce2de0a49674a268590fb3725354bad0e08d
kanell0304 <kanell0304@gmail.com>
2025-11-12 23:13:05 +0900

    moved files

---
+++ frontend/src/usePred.jsx
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const API_BASE_URL = 'http://localhost:8081'
+
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+
+export const usePred = () => {
+
+  const getPred = async (path) => {
+    try {
+      const response = await api.post('/predict', { path })
+      return response.data
+    } catch (err) {
+      console.error(err.response?.data || err.message)
+      throw err 
+    }
+  }
+  return {getPred}
+}
